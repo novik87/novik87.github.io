f5690900cae9d5a87254915afb0ac0d5ff2eca40
novik87 <79408900+novik87@users.noreply.github.com>
2021-02-24 18:31:05 +0300

    Добавил файлы сайта sale
    Добавил файлы сайта sale

---
+++ sale.lider-krovlya.ru/js/form.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function(){
+	jQuery("form").submit(function() { // Событие отправки с формы
+		var form_data = jQuery(this).serialize(); // Собираем данные из полей
+		jQuery.ajax({
+			type: "POST", // Метод отправки
+			url: "sendform.php", // Путь к PHP обработчику sendform.php
+			data: form_data,
+			success: swal({
+				title: "Дынные успешно отправлены, Спасибо за заявку!",
+                type: "success",
+                showConfirmButton: false,
+                timer: 3000
+            })
+        });
+        $(this).find('input, textarea').prop('disabled', true);
+        event.preventDefault();
+    });
+});
+
